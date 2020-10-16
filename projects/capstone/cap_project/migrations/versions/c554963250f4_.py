19f53691e218defea1b3a3831ed38996b211d6fb
olegpolivin <olegpolivin@gmail.com>
2020-10-16 22:36:12 +0200

    Correcting downgrade version for migrations 3

--- projects/capstone/cap_project/migrations/versions/c554963250f4_.py
+++ projects/capstone/cap_project/migrations/versions/c554963250f4_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c554963250f4'
-down_revision = 'e6706378eab9'
+down_revision = 'a180e07e8116'
 branch_labels = None
 depends_on = None
 
