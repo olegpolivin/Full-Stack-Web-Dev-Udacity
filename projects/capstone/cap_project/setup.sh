9c8163b574da5c2161a90e276fd83374ea7b6d19
olegpolivin <olegpolivin@gmail.com>
2020-10-16 22:59:33 +0200

    check ENV vars

--- projects/capstone/cap_project/setup.sh
+++ projects/capstone/cap_project/setup.sh
@@ -1,4 +1,3 @@
 export AUTH0_DOMAIN = 'onlineeducation.eu.auth0.com'
 export ALGORITHMS = ['RS256']
 export API_AUDIENCE = 'CourseAndDomain'
-export DATABASE_URL = 'postgres://prcupmwbpkaota:844d7842df4c714eb0fb01254352446d3d6aae262f8e7dc2679b7ed1e2884327@ec2-52-87-22-151.compute-1.amazonaws.com:5432/d2vq33eg241ojl'
